f7ebc1c552b999c9183d519125980b9c3e2cd60d
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2025-10-27 12:53:52 +0100

    pseudo code

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+console.log('It works');
+
+//Create a ajax call to get the pictures array
+//-Save the array
+
+
+//Save the row domEl
+
+//Cycle into the array to generate a card with a picture
+//-ForEach cycle
+//-Save custom card markup
+//-Save the new cards into the rowEl
+
