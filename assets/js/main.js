d5651b1807e94d776c21cb3277061a8dba0c7a72
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2025-10-27 14:13:51 +0100

    Create genCards function

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,37 +4,43 @@ console.log('It works');
 const rowEl = document.querySelector('.row')
 console.log(rowEl);
 
-//Create a ajax call to get the pictures array
-const pictures = []
-fetch('https://lanciweb.github.io/demo/api/pictures/')
-    .then(response => response.json())
-    .then(data => {
-        //-Save the array
-        for (let i = 0; i < 6; i++) {
-            const element = data[i];
-            pictures.push(element)
-        }
-    })
-console.log(pictures);
 
 
 //Create a function to generate a card for each picture
-function genCard(picture) {
-    //Cycle into the array to generate a card with a picture
-    const card = `<div class="col">
-    <div class="mycard">
-    <img src="${picture}" alt="">
-    <div>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-    </div>
-    <img class="pin" src="./assets/img/pin.svg" alt="">
-    </div>`
-    
-}
-//-Save custom card markup
-//-ForEach cycle
+/**
+ * generate cards based on array's object and stamp in the selected El
+ * @param {*} 
+*/
+function genCards() {
 
-//-Save the new cards into the rowEl
-//Call the function to generate cards
+    //Create a ajax call to get the pictures array
+
+    //Cycle into the array to generate a card with a picture
+    //-ForEach cycle
+    for (let i = 0; i < 6; i++) {
+        let card
+        //-Save custom card markup
+
+        fetch('https://lanciweb.github.io/demo/api/pictures/')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data[i].url);
+
+                card = `<div class="col">
+                <div class="mycard">
+                <img src="${data[i].url}" alt="">
+                <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </div>
+                <img class="pin" src="./assets/img/pin.svg" alt="">
+                </div>`
+                //Stamp in page
+                rowEl.innerHTML += card
+            })
+    };
+
+}
+
+genCards()
 
 
