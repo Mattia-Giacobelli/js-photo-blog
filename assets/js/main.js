f75b9d968cf72f7ac2974ba03dd89dbfe64c4ca7
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2025-10-28 17:54:16 +0100

    Create overlay

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,8 @@ console.log(rowEl);
 //Save pictures array
 const pictures = []
 
+
+
 //Create a ajax call to get the pictures array
 fetch('https://lanciweb.github.io/demo/api/pictures/')
     .then(response => response.json())
@@ -16,6 +18,65 @@ fetch('https://lanciweb.github.io/demo/api/pictures/')
         });
         stampCard(pictures, rowEl)
         console.log(pictures);
+
+        //Select cards El and overlay
+        const skateCard = document.getElementById('1')
+        const passeggiataCard = document.getElementById('2')
+        const alpiCard = document.getElementById('3')
+        const sagraCard = document.getElementById('4')
+        const waterCard = document.getElementById('5')
+        const rivieraCard = document.getElementById('6')
+        const overlay = document.getElementById('overlay')
+        const ovBtn = document.querySelector('button')
+        console.log(skateCard, overlay);
+
+        skateCard.addEventListener('click', () => {
+            //const img = document.getElementsByClassName('1')
+            overlay.style = "display: block"
+            const ovImg = document.querySelector('.ov-img')
+            ovImg.src = img.src
+
+        })
+        passeggiataCard.addEventListener('click', () => {
+            //const img = document.getElementsByClassName('1')
+            overlay.style = "display: block"
+            const ovImg = document.querySelector('.ov-img')
+            ovImg.src = img.src
+
+        })
+        alpiCard.addEventListener('click', () => {
+            //const img = document.getElementsByClassName('1')
+            overlay.style = "display: block"
+            const ovImg = document.querySelector('.ov-img')
+            ovImg.src = img.src
+
+        })
+        sagraCard.addEventListener('click', () => {
+            //const img = document.getElementsByClassName('1')
+            overlay.style = "display: block"
+            const ovImg = document.querySelector('.ov-img')
+            ovImg.src = img.src
+
+        })
+        waterCard.addEventListener('click', () => {
+            //const img = document.getElementsByClassName('1')
+            overlay.style = "display: block"
+            const ovImg = document.querySelector('.ov-img')
+            ovImg.src = img.src
+
+        })
+        rivieraCard.addEventListener('click', () => {
+            //const img = document.getElementsByClassName('1')
+            overlay.style = "display: block"
+            const ovImg = document.querySelector('.ov-img')
+            ovImg.src = img.src
+
+        })
+
+        ovBtn.addEventListener('click', (e) => {
+            e.preventDefault()
+            overlay.style = "display: none"
+        })
     })
 
 
@@ -29,7 +90,7 @@ function stampCard(array, El) {
 
         let card = `<div class="col">
             <div class="mycard" id="${pic.id}">
-            <img src="${pic.url}" alt="">
+            <img class="${pic.id}" src="${pic.url}" alt="">
             <div class="date">
             ${pic.date}
             </div>
@@ -49,17 +110,12 @@ function stampCard(array, El) {
 
 //Open/close the overlay
 
+
 //Create a function to change overlay's display property
+
 
 //Create a function to get the card img src and stamp into the overlay
 
 //Create event listener for each card and call the previous functions
-const skateCard = document.getElementById('1')
-const overlay = document.getElementById('overlay')
-console.log(skateCard, overlay);
 
 
-// skateCard.addEventListener('click', () => {
-//     console.log('ciao');
-
-// })
